7388e44bf3f0cf696521879be1473ed792e452c6
Archanagowda23 <archanar249@gmail.com>
2022-03-24 19:02:17 +0530

    added generic function to find max of 3 strings

--- src/com/bridgelabz/generic/MaximumTest.java
+++ src/com/bridgelabz/generic/MaximumTest.java
@@ -12,4 +12,20 @@ public class MaximumTest<T extends Comparable<T>> {
         this.z = z;
     }
 
+    public static String testMaximum(String x, String y, String z) {
+        String max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        printMax(x, y, z, max);
+        return max;
     }
+
+    public static <T> void printMax(T x, T y, T z, T max) {
+        System.out.printf("Max of %s, %s and %s is %s\n", x, y, z, max);
+
+    }
+}
--- src/com/bridgelabz/generic/Test.java
+++ src/com/bridgelabz/generic/Test.java
@@ -7,5 +7,9 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("Welcome to generic max-numbers program");
+
+        String xStr = "zmelon", yStr = "apple", zStr = "grape";
+
+        MaximumTest.testMaximum(xStr,yStr,zStr);
     }
 }
